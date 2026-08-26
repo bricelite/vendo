ab4142c639f1c7e3b2735c838e1c6e4e135138dc
bricelite <melkiorzounon@gmail.com>
2026-08-26 21:10:41 +0100

    Fix: clean packages.php (no dev deps), cache env vars, debug logging, APP_STORAGE redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -14,5 +14,20 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (\Throwable $e) {
+            \Log::error('VERCEL_DEBUG_ORIGINAL: ' . $e::class . ': ' . $e->getMessage(), [
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ]);
+            \Log::error('VERCEL_DEBUG_TRACE: ' . $e->getTraceAsString());
+        });
     })->create();
+
+if (env('APP_STORAGE')) {
+    $app->useStoragePath(env('APP_STORAGE'));
+    foreach (['framework/views', 'framework/cache/data', 'app/private/uploads', 'logs'] as $dossier) {
+        @mkdir(storage_path($dossier), 0775, true);
+    }
+}
+
+return $app;
